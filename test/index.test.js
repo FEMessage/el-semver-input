06c8bd0681acc57bd3cf6379b66b9debbf5a7968
EVILLT <ijoec123@gmail.com>
2019-07-04 16:36:51 +0800

    chore: use vue-sfc-cli upgrade (#8)

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,5 @@
-import {shallowMount} from '@vue/test-utils'
-import Component from '../src'
+import sum from './sum'
 
-describe('Component', () => {
-  it('is a Vue instance', () => {
-    const wrapper = shallowMount(Component)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
+test('main', () => {
+  expect(sum(1, 2)).toBe(3)
 })
